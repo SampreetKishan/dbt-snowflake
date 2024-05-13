951fb5f4592cec8a3220f8d0a397fb02ed8cb241
SampreetKishan <sampy.prithvi2@gmail.com>
2024-05-13 21:47:47 +0000

    Added dbt tests

--- models/stg_customers.sql
+++ models/stg_customers.sql
@@ -2,4 +2,4 @@
             id as customer_id,
             first_name,
             last_name
-        from dbt_raw.jaffle_shop.customers 
+        from {{ source('jaffle_shop','customers') }}
--- models/stg_orders.sql
+++ models/stg_orders.sql
@@ -3,4 +3,4 @@
             user_id as customer_id,
             order_date,
             status
-        from dbt_raw.jaffle_shop.orders
+        from {{ source('jaffle_shop','orders')}}
